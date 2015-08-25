e929cb5d24fec64b77453e4a42b014b8801cf030
mattheh <mattheh@email.unc.edu>
2015-08-25 12:11:54 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    resources :posts
+  end
 
   root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
