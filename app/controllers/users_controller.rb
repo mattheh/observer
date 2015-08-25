3ef222a99494a39c6a199de10411888c0fb1bcd3
mattheh <mattheh@email.unc.edu>
2015-08-25 11:47:18 -0400

    Update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
