a2e974f37dc2dbfc9891dbc5a3d0129dad9fac75
mattheh <mattheh@email.unc.edu>
2015-08-25 11:56:27 -0400

    Create User and Post model

---
+++ db/migrate/20150825155456_create_posts.rb
@@ -0,0 +1,15 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.integer :price
+      t.string :category
+      t.string :location
+      t.striring :url
+      t.boolean :active
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
