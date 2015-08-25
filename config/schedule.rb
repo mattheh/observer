6031d229d1f650b5df6680bf7e23aa58a8051dd2
mattheh <mattheh@email.unc.edu>
2015-08-25 15:14:08 -0400

    Add test job in schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,6 @@
+every 2.minutes do
+  rake "posts:run"
+end
 # Use this file to easily define all of your cron jobs.
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
