df6a4aae644a4b54f3fdce01c70399f4ba2b07c6
mattheh <mattheh@email.unc.edu>
2015-08-28 15:52:40 -0400

    Add `new_posting?` method

--- app/models/observer/runner.rb
+++ app/models/observer/runner.rb
@@ -2,24 +2,28 @@ require 'open-uri'
 module Observer
   module Runner
     def self.run
-      get_urls.each do |url|
-        page = Nokogiri::HTML(open(url))   
-        puts page.class
+      User.all.each do |user|
+        to_mail = []
+        user.posts.each do |post|
+          page = Nokogiri::HTML(open(get_url(post)))   
+          page.css("p").each do |link|
+            begin
+              break if !new_posting?(post, link)
+              (to_mail << link.css("a").attr("href").value) if (link.css("span.price").first.inner_text.split("$")[1].to_i < post.price)
+            rescue
+            else
+            end
+          end
+        end
+        puts to_mail
       end
     end  
 
-    def self.get_urls
-      info = []
-      User.all.each do |user|
-        user.posts.each do |post|
-          title = post.title.split.join("+")
-          category = codename(post.category)
-          location = post.location.downcase
-          url = "https://#{location}.craigslist.org/search/#{category}?sort=date&query=#{title}"
-          info << url
-        end
-      end
-      return info
+    def self.get_url(post)
+      title = post.title.split.join("+")
+      category = codename(post.category)
+      location = post.location.downcase
+      url = "https://#{location}.craigslist.org/search/#{category}?sort=date&query=#{title}"
     end
 
     def self.codename(name)
@@ -30,6 +34,14 @@ module Observer
     end
     def self.test
       puts "TEST"
+    end
+
+    def self.new_posting?(post, link)
+      location = post.location.downcase
+      inner_page = Nokogiri::HTML(open("https://#{location}.craigslist.org#{link.css("a").attr("href").value}"))
+      post_time = Time.parse(inner_page.css("p").children.css("time").last.attr("datetime"))
+      return false if (Time.now - post_time) > 300
+      return true
     end
   end
 end
