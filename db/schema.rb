f6e19ca12f81b80a300fd76882b898b6af8a2c4f
mattheh <mattheh@email.unc.edu>
2015-08-28 16:15:41 -0400

    Add Email Column to :users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825155456) do
+ActiveRecord::Schema.define(version: 20150828201240) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150825155456) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
   end
 
 end
